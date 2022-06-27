98bcaa1b12b5372f8ceccb894def0fbdf70327d2
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-06-27 13:29:51 -0400

    Add starter logic for removing book cards

--- script.js
+++ script.js
@@ -15,27 +15,31 @@ inFormButton.addEventListener('click', event => {
 });
 
 
-let myLibrary = [{
-    title: "title1",
-    author: "author1",
-    dateWritten: 1970,
-    pageCount: 100,
-    read: "yes"
-},
-{
-    title: "title2",
-    author: "author2",
-    dateWritten: 1970,
-    pageCount: 100,
-    read: "yes"
-},
-{
-    title: "title3",
-    author: "author3",
-    dateWritten: 1970,
-    pageCount: 100,
-    read: "yes"
-}];
+
+
+let myLibrary = [
+//     {
+//     title: "title1",
+//     author: "author1",
+//     dateWritten: 1970,
+//     pageCount: 100,
+//     read: "yes"
+// },
+// {
+//     title: "title2",
+//     author: "author2",
+//     dateWritten: 1970,
+//     pageCount: 100,
+//     read: "yes"
+// },
+// {
+//     title: "title3",
+//     author: "author3",
+//     dateWritten: 1970,
+//     pageCount: 100,
+//     read: "yes"
+// }
+];
 
 function Book(title, author, dateWritten, pageCount, read) {
   // the constructor...
@@ -59,16 +63,25 @@ function createCardStack(){
         let card = document.createElement("div");
         card.classList.add("card");
         for (key in book){
-            card.appendChild(document.createElement("p")).appendChild(document.createTextNode(book[key]));
+            // card.appendChild(document.createElement("p")).appendChild(document.createTextNode(book[key]));
+            let element = document.createElement("p");
+            element.appendChild(document.createTextNode(book[key]))
+            if (key == "title"){
+                element.classList.add("title");
+            }
+            card.appendChild(element);
         }
-        let deleteButton = document.createElement("button");
-        deleteButton.classList.add("deleteBookButton");
-        deleteButton.textContent = "Remove";
-        card.appendChild(deleteButton);
-        cardContainer.appendChild(card); 
-    });
-
+        let deleteBookCardButton = document.createElement("button");
+        deleteBookCardButton.classList.add("deleteBookCardButton");
+        deleteBookCardButton.textContent = "Remove";
+        deleteBookCardButton.addEventListener('click', removeBookCard);
+        card.appendChild(deleteBookCardButton);
+        cardContainer.appendChild(card);
+        
+    }); 
 }
+
+
 
 function closeForm() {
     document.getElementById("bookForm").style.display = "none";
@@ -76,4 +89,33 @@ function closeForm() {
 
 function openForm() {
     document.getElementById("bookForm").style.display = "block";;
+}
+
+function removeBookCard(event) {
+    let bookTitle = event.currentTarget.parentNode.children[0].textContent;
+    // console.log(bookTitle);
+    // console.log(myLibrary);
+    // let index = myLibrary.indexOf(bookTitle);
+    // console.log(index);
+
+    // let obj = myLibrary.find(book => book.title === bookTitle);
+    // console.log(obj);
+
+    console.log(myLibrary);
+
+    myLibrary = myLibrary.filter(function( obj ) {
+        return obj.title !== bookTitle;
+    });
+
+    console.log(myLibrary);
+
+    createCardStack();
+
+    // myLibrary.forEach(book => {
+    //     console.log(book);
+    //     for (let key in book) {
+    //         console.log(key);
+    //         // console.log(`${key}: ${book[key]}`);
+    //     }
+    // });
 }
